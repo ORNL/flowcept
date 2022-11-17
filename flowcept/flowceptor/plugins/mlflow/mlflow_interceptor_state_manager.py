abaa93f03248b0a2750e1fe621693e5239075060
Renan Souza <contact@renansouza.org>
2022-11-17 09:24:05 -0500

    adding -d in docker compose gh actions

--- flowcept/flowceptor/plugins/mlflow/mlflow_interceptor_state_manager.py
+++ flowcept/flowceptor/plugins/mlflow/mlflow_interceptor_state_manager.py
@@ -4,4 +4,7 @@ from flowcept.flowceptor.plugins.abstract_interceptor_state_manager import (
 
 
 class MLFlowInterceptorStateManager(AbstractInterceptorStateManager):
+    # TODO: check if we really need this class.
+    # Only reason we would need this is if we want to have specific methods
+    # for this class.
     pass
