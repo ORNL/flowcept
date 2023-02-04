780f58bd467449d0a3b1f517fbff92c55b4e813a
Renan Souza <contact@renansouza.org>
2023-02-04 17:08:15 -0500

    Zambeze + Dask tests passing

--- flowcept/flowcept_consumer/doc_db/document_db_dao.py
+++ flowcept/flowcept_consumer/doc_db/document_db_dao.py
@@ -1,4 +1,4 @@
-from typing import List, Dict, Optional
+from typing import List, Dict
 from bson import ObjectId
 from pymongo import MongoClient, UpdateOne
 
@@ -9,58 +9,7 @@ from flowcept.configs import (
     MONGO_COLLECTION,
 )
 
-from flowcept.commons.flowcept_data_classes import TaskMessage, Status
-
-
-def curate_task_messages(doc_list: List[Dict],
-                         indexing_key: str):
-    """
-       This function removes duplicates based on the
-        indexing_key (e.g., task_id) locally before sending
-        to MongoDB.
-        It also avoids tasks changing states once they go into finished state.
-        This is needed because we can't guarantee MQ orders, and finished
-        states have higher priority in status changes, as we don't expect a
-        status change once a task goes into finished state.
-        It also resolves updates (instead of replacement) of
-        inner nested fields in a JSON object.
-    :param doc_list:
-    :param indexing_key:
-    :return:
-    """
-    indexed_buffer = {}
-    for doc in doc_list:
-
-        if (len(doc) == 1) and (indexing_key in doc) and (doc[indexing_key] in indexed_buffer):
-            # This task_msg does not add any metadata
-            continue
-
-        indexing_key_value = doc[indexing_key]
-        if doc[indexing_key] not in indexed_buffer:
-            indexed_buffer[indexing_key_value] = doc
-            continue
-
-        if 'finished' in indexed_buffer[indexing_key_value] and 'status' in doc:
-            doc.pop('status')
-
-        if 'status' in doc:
-            for finished_status in Status.get_finished_statuses():
-                if finished_status == doc['status']:
-                    indexed_buffer[indexing_key_value]['finished'] = True
-
-        for field in TaskMessage.get_dict_field_names():
-            if field in doc:
-                if doc[field] is not None and len(doc[field]):
-                    if field in indexed_buffer[indexing_key_value]:
-                        indexed_buffer[indexing_key_value][
-                            field].update(doc[field])
-                    else:
-                        indexed_buffer[indexing_key_value][field] = \
-                        doc[field]
-                doc.pop(field)
-
-        indexed_buffer[indexing_key_value].update(**doc)
-    return indexed_buffer
+from flowcept.flowcept_consumer.consumer_utils import curate_dict_task_messages
 
 
 class DocumentDBDao(object):
@@ -96,18 +45,13 @@ class DocumentDBDao(object):
             print("Error when inserting many docs", e, str(doc_list))
             return None
 
-    def insert_and_update_many(self, indexing_key, doc_list: List[Dict], nested_fields: Optional[List[str]] = None) -> bool:
+    def insert_and_update_many(self, indexing_key, doc_list: List[Dict]) -> bool:
         try:
-            indexed_buffer = curate_task_messages(
+            indexed_buffer = curate_dict_task_messages(
                 doc_list,
                 indexing_key
             )
-        except Exception as e:
-            print("Error when updating or inserting docs", e, str(doc_list))
-            return False
-
-        requests = []
-        try:
+            requests = []
             for indexing_key_value in indexed_buffer:
                 if "finished" in indexed_buffer[indexing_key_value]:
                     indexed_buffer[indexing_key_value].pop('finished')
