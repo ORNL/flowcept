14c5943c3bed992238c73c6dbc296b81b0fc775a
Renan Souza <contact@renansouza.org>
2023-02-08 12:16:19 -0500

    log tests

---
+++ tests/log_tests.py
@@ -0,0 +1,10 @@
+tests/log_tests.pyfrom flowcept.commons.flowcept_logger import logger
+
+try:
+    logger.debug("debug")
+    logger.info("info")
+    logger.error("info")
+    x = 2/0
+except Exception as e:
+    logger.exception(e)
+    logger.info("It's ok")
