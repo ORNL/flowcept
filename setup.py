0384d9d02fcf96d1aa26688aab9524baedeff741
Renan Souza <contact@renansouza.org>
2023-02-12 09:13:27 -0500

    Python38 support

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from sys import platform
+import os
 from setuptools import setup, find_packages
 
 with open("flowcept/version.py") as f:
@@ -29,15 +30,20 @@ _EXTRA_REQUIREMENTS = [
     "mongo",
     "dask",
     "webserver",
-    "api",
 ]
 
-MAC_REQUIRES = []
+MAC_REQUIRES = ["tensorboard"]
 
 extras_requires = dict()
 for req in _EXTRA_REQUIREMENTS:
     if req in MAC_REQUIRES and platform == "darwin":
         req_path = f"extra_requirements/{req}-requirements-mac.txt"
+        # These env vars are needed to install tensorflow on mac
+        # (at least on m1 chip)
+        # (because of the grpcio package). See:
+        # https://stackoverflow.com/questions/66640705/how-can-i-install-grpcio-on-an-apple-m1-silicon-laptop
+        os.environ["GRPC_PYTHON_BUILD_SYSTEM_OPENSSL"] = "1"
+        os.environ["GRPC_PYTHON_BUILD_SYSTEM_ZLIB"] = "1"
     else:
         req_path = f"extra_requirements/{req}-requirements.txt"
     extras_requires[req] = get_requirements(req_path)
@@ -62,16 +68,13 @@ setup(
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.8",
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
         "Natural Language :: English",
-        "Topic :: Documentation :: Sphinx",
+        # "Topic :: Documentation :: Sphinx",
         "Topic :: System :: Distributed Computing",
     ],
-    python_requires=">=3.10",  # TODO: We don't really need py3.10,
-    # but if we change it now,
-    # we'll need to change CI/CD tests
+    python_requires=">=3.8",
     # scripts=["bin/flowcept"],
 )
