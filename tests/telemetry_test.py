6b9d6a1c6b371e44371f700ebd8cf33f2c86b7eb
Renan Souza <contact@renansouza.org>
2023-06-29 17:40:05 -0400

    Adding native telemetry

---
+++ tests/telemetry_test.py
@@ -0,0 +1,9 @@
+import unittest
+from flowcept.flowceptor.telemetry_capture import capture
+
+
+class TestTelemetry(unittest.TestCase):
+
+    def test_telemetry(self):
+        telemetry = capture()
+        assert telemetry.to_dict()
