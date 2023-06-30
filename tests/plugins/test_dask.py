4506e59d8c90fcd7087e9a79dadb925849314f5a
Renan Souza <contact@renansouza.org>
2023-06-30 13:21:16 -0400

    Minor fix in setup.py

--- tests/plugins/test_dask.py
+++ tests/plugins/test_dask.py
@@ -137,7 +137,9 @@ class TestDask(unittest.TestCase):
         print("Task_id=" + o2_task_id)
         print("Done workflow!")
         sleep(3)
-        assert len(doc_dao.query({"task_id": o2_task_id})) > 0
+        docs = doc_dao.query({"task_id": o2_task_id})
+        assert len(docs) > 0
+        assert len(docs[0]["telemetry_at_end"]) > 0
 
     def test_observer_and_consumption_varying_args(self):
         doc_dao = DocumentDBDao()
