630a0a4c6a42eccaea4af833112c9359305fdcd2
Renan Souza <contact@renansouza.org>
2023-06-30 13:12:31 -0400

    Code formatting

--- tests/plugins/test_dask.py
+++ tests/plugins/test_dask.py
@@ -134,7 +134,7 @@ class TestDask(unittest.TestCase):
     def test_observer_and_consumption(self):
         doc_dao = DocumentDBDao()
         o2_task_id = self.atest_pure_workflow()
-        print("Task_id="+o2_task_id)
+        print("Task_id=" + o2_task_id)
         print("Done workflow!")
         sleep(3)
         assert len(doc_dao.query({"task_id": o2_task_id})) > 0
