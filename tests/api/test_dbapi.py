5356797f1963e90ea1e9664e880aa461906757f6
Renan Souza <contact@renansouza.org>
2023-09-22 18:32:28 -0400

    Fixing custom_metadata inner updates

--- tests/api/test_dbapi.py
+++ tests/api/test_dbapi.py
@@ -6,25 +6,34 @@ from flowcept.flowcept_api.db_api import DBAPI
 class WorkflowDBTest(unittest.TestCase):
     def test_wf_dao(self):
         dbapi = DBAPI()
-        assert dbapi.insert_or_update_workflow(workflow_id="wftest")
+        wf1 = str(uuid4())
 
         assert dbapi.insert_or_update_workflow(
-            workflow_id="wftest2",
+            workflow_id=wf1,
             custom_metadata={"bla": "blu"},
             comment="comment test",
         )
 
-        assert dbapi.insert_or_update_workflow(workflow_id="wftest2")
+        assert dbapi.insert_or_update_workflow(
+            workflow_id=wf1, custom_metadata={"bli": "blo"}
+        )
 
-        wfdata = dbapi.get_workflow(workflow_id="wftest2")
+        wfdata = dbapi.get_workflow(workflow_id=wf1)
         assert wfdata is not None
         print(wfdata)
 
-        wf3 = str(uuid4())
+        wf2 = str(uuid4())
 
-        assert dbapi.insert_or_update_workflow(workflow_id=wf3)
+        assert dbapi.insert_or_update_workflow(workflow_id=wf2)
         assert dbapi.insert_or_update_workflow(
-            workflow_id=wf3, comment="test"
+            workflow_id=wf2, comment="test"
         )
-
-        assert dbapi.get_workflow(workflow_id=wf3)
+        assert dbapi.insert_or_update_workflow(
+            workflow_id=wf2, custom_metadata={"a": "b"}
+        )
+        assert dbapi.insert_or_update_workflow(
+            workflow_id=wf2, custom_metadata={"c": "d"}
+        )
+        wfdata = dbapi.get_workflow(workflow_id=wf2)
+        assert wfdata is not None
+        assert len(wfdata["custom_metadata"]) == 2
