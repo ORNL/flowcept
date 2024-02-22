823631bf8c80e77d07cd76d774bdfd22a3f658e4
Renan Souza <contact@renansouza.org>
2024-02-21 21:13:41 -0500

    Refactor to remove llm from tests

---
+++ tests/decorator_tests/ml_tests/llm/decorator_dask_llm_test.py
@@ -0,0 +1,77 @@
+import unittest
+
+import uuid
+from uuid import uuid4
+
+from dask.distributed import Client
+
+from cluster_experiment_utils.utils import generate_configs
+
+from flowcept import FlowceptConsumerAPI, WorkflowObject, TaskQueryAPI
+
+from flowcept.commons.flowcept_logger import FlowceptLogger
+
+from flowcept.flowcept_api.db_api import DBAPI
+from tests.decorator_tests.ml_tests.dl_trainer import ModelTrainer
+from tests.adapters.test_dask import TestDask
+from tests.decorator_tests.ml_tests.llm.llm_trainer import (
+    get_wiki_text,
+    model_train,
+)
+
+
+class DecoratorDaskTests(unittest.TestCase):
+    client: Client = None
+    consumer: FlowceptConsumerAPI = None
+
+    def __init__(self, *args, **kwargs):
+        super(DecoratorDaskTests, self).__init__(*args, **kwargs)
+        self.logger = FlowceptLogger()
+
+    @classmethod
+    def setUpClass(cls):
+        TestDask.client = TestDask._setup_local_dask_cluster(n_workers=1)
+
+    def test_llm(self):
+        ntokens, train_data, val_data, test_data = get_wiki_text()
+
+        wf_id = str(uuid.uuid4())
+        print(f"Workflow_id={wf_id}")
+        exp_param_settings = {
+            "batch_size": [20],
+            "eval_batch_size": [10],
+            "emsize": [200],
+            "nhid": [200],
+            "nlayers": [2],  # 2
+            "nhead": [2],
+            "dropout": [0.2],
+            "epochs": [1, 3],
+            "lr": [0.1, 0.01],
+            "pos_encoding_max_len": [5000],
+        }
+        configs = generate_configs(exp_param_settings)
+        outputs = []
+        for conf in configs:
+            conf.update(
+                {
+                    "ntokens": ntokens,
+                    "train_data": train_data,
+                    "val_data": val_data,
+                    "test_data": test_data,
+                    "workflow_id": wf_id,
+                }
+            )
+            outputs.append(TestDask.client.submit(model_train, **conf))
+        for o in outputs:
+            o.result()
+
+    @classmethod
+    def tearDownClass(cls):
+        print("Closing scheduler and workers!")
+        try:
+            TestDask.client.shutdown()
+        except:
+            pass
+        print("Closing flowcept!")
+        if TestDask.consumer:
+            TestDask.consumer.stop()
--- tests/decorator_tests/ml_tests/ml_decorator_tests.py
+++ tests/decorator_tests/ml_tests/ml_decorator_tests.py
@@ -1,9 +1,7 @@
 import uuid
-from time import sleep
 
 import numpy as np
 import torch
-from torch import nn
 
 import flowcept.commons
 import flowcept.instrumentation.decorators
@@ -11,11 +9,10 @@ from flowcept import model_explainer, model_profiler, FlowceptConsumerAPI
 
 import unittest
 
-from flowcept.instrumentation.decorators.flowcept_task import flowcept_task
 from tests.decorator_tests.ml_tests.dl_trainer import (
     ModelTrainer,
 )
-from tests.decorator_tests.ml_tests.llm_trainer import (
+from tests.decorator_tests.ml_tests.llm.llm_trainer import (
     model_train,
     get_wiki_text,
     TransformerModel,
@@ -59,67 +56,3 @@ class MLDecoratorTests(unittest.TestCase):
                 conf["workflow_id"] = wf_id
                 result = trainer.model_fit(**conf)
                 print(result)
-
-    @staticmethod
-    def test_llm_model_trainer():
-        ntokens, train_data, val_data, test_data = get_wiki_text()
-        wf_id = str(uuid.uuid4())
-        # conf = {
-        #      Original
-        #     "batch_size": 20,
-        #     "eval_batch_size": 10,
-        #     "emsize": 200,
-        #     "nhid": 200,
-        #     "nlayers": 2, #2
-        #     "nhead": 2,
-        #     "dropout": 0.2,
-        #     "epochs": 3,
-        #     "lr": 0.001,
-        #     "pos_encoding_max_len": 5000
-        # }
-
-        conf = {
-            "batch_size": 20,
-            "eval_batch_size": 10,
-            "emsize": 200,
-            "nhid": 200,
-            "nlayers": 2,  # 2
-            "nhead": 2,
-            "dropout": 0.2,
-            "epochs": 1,
-            "lr": 0.1,
-            "pos_encoding_max_len": 5000,
-        }
-        conf.update(
-            {
-                "ntokens": ntokens,
-                "train_data": train_data,
-                "val_data": val_data,
-                "test_data": test_data,
-                "workflow_id": wf_id,
-            }
-        )
-        result = model_train(**conf)
-        assert result
-        print(MLDecoratorTests.debug_model_profiler(conf, ntokens, test_data))
-
-    @staticmethod
-    @model_profiler()
-    def debug_model_profiler(conf, ntokens, test_data):
-        best_m = TransformerModel(
-            ntokens,
-            conf["emsize"],
-            conf["nhead"],
-            conf["nhid"],
-            conf["nlayers"],
-            conf["dropout"],
-        ).to("cpu")
-        m = torch.load("transformer_wikitext2.pth")
-        best_m.load_state_dict(m)
-        return {
-            "test_loss": 0.01,
-            "train_loss": 0.01,
-            "val_loss": 0.01,
-            "model": best_m,
-            "test_data": test_data,
-        }
