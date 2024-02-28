04a57399987b587143816a9545e19aee3df51f6f
Renan Souza <contact@renansouza.org>
2024-02-28 11:02:12 -0500

    Creating indices only if they don't exist

--- flowcept/commons/daos/document_db_dao.py
+++ flowcept/commons/daos/document_db_dao.py
@@ -39,20 +39,32 @@ class DocumentDBDao(object):
         else:
             client = MongoClient(MONGO_HOST, MONGO_PORT)
         self._db = client[MONGO_DB]
-        existing_collections = self._db.list_collection_names()
 
         self._tasks_collection = self._db[MONGO_TASK_COLLECTION]
         self._wfs_collection = self._db[MONGO_WORKFLOWS_COLLECTION]
 
-        # Creation of indices
-        if MONGO_TASK_COLLECTION not in existing_collections:
+        self._create_indices()
+
+    def _create_indices(self):
+        # Creating task collection indices:
+        existing_indices = [
+            list(x["key"].keys())[0]
+            for x in self._tasks_collection.list_indexes()
+        ]
+        if not TaskObject.task_id_field() in existing_indices:
             self._tasks_collection.create_index(TaskObject.task_id_field())
+        if not TaskObject.workflow_id_field() in existing_indices:
             self._tasks_collection.create_index(
                 TaskObject.workflow_id_field()
             )
 
-        if MONGO_WORKFLOWS_COLLECTION not in existing_collections:
-            self._wfs_collection.create_index(TaskObject.workflow_id_field())
+        # Creating workflow collection indices:
+        existing_indices = [
+            list(x["key"].keys())[0]
+            for x in self._wfs_collection.list_indexes()
+        ]
+        if not TaskObject.workflow_id_field() in existing_indices:
+            self._wfs_collection.create_index(TaskObject.task_id_field())
 
     def task_query(
         self,
