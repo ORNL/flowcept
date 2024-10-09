4c270fe6c0146a98d02c6787e6db48e50caa2dd4
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2024-10-08 21:59:10 -0400

    Fix lint and format warnings for flowcept_webserver subpackage

--- src/flowcept/flowcept_webserver/app.py
+++ src/flowcept/flowcept_webserver/app.py
@@ -1,5 +1,7 @@
+"""App module."""
+
 from flask_restful import Api
-from flask import Flask, request, jsonify
+from flask import Flask
 
 from flowcept.configs import WEBSERVER_HOST, WEBSERVER_PORT
 from flowcept.flowcept_webserver.resources.query_rsrc import TaskQuery
@@ -18,6 +20,7 @@ api.add_resource(TaskQuery, f"{BASE_ROUTE}/{TaskQuery.ROUTE}")
 
 @app.route("/")
 def liveness():
+    """Livelyness string."""
     return "Server up!"
 
 
