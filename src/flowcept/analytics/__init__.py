f6130577273a41007067712591dc4fdc06c995e1
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2024-10-16 11:17:12 -0400

    Fix lint and format warnings for analytics subpackage

--- src/flowcept/analytics/__init__.py
+++ src/flowcept/analytics/__init__.py
@@ -1,3 +1,5 @@
+"""Analytics subpackage."""
+
 from flowcept.analytics.analytics_utils import (
     clean_dataframe,
     analyze_correlations_used_vs_generated,
@@ -8,3 +10,14 @@ from flowcept.analytics.analytics_utils import (
     describe_col,
     describe_cols,
 )
+
+__all__ = [
+    "clean_dataframe",
+    "analyze_correlations_used_vs_generated",
+    "analyze_correlations",
+    "analyze_correlations_generated_vs_telemetry_diff",
+    "analyze_correlations_used_vs_telemetry_diff",
+    "analyze_correlations_between",
+    "describe_col",
+    "describe_cols",
+]
--- src/flowcept/analytics/analytics_utils.py
+++ src/flowcept/analytics/analytics_utils.py
@@ -1,6 +1,7 @@
+"""Analytics utility module."""
+
 import logging
 import numbers
-
 import numpy as np
 import pandas as pd
 
@@ -10,12 +11,14 @@ _CORRELATION_DF_HEADER = ["col_1", "col_2", "correlation"]
 
 
 def is_list(val):
+    """Check if list."""
     if type(val) in {list, np.array, pd.Series}:
         return True
     return False
 
 
 def flatten_list_with_sum(val):
+    """Flatten list with sum."""
     _sum = 0
     if is_list(val):
         for el in val:
@@ -39,7 +42,7 @@ def clean_dataframe(
     sum_lists=False,
     aggregate_telemetry=False,
 ) -> pd.DataFrame:
-    """
+    """Clean the dataframe.
 
     :param sum_lists:
     :param keep_task_id:
@@ -49,12 +52,11 @@ def clean_dataframe(
     :param logger:
     :param keep_telemetry_percent_columns:
     :param aggregate_telemetry: We use some very simplistic forms of aggregations just
-     to reduce the complexity of the dataframe. Use this feature very carefully as the aggregation may be misleading.
+      to reduce the complexity of the dataframe. Use this feature very carefully as the
+      aggregation may be misleading.
     :return:
     """
-    has_telemetry_diff_columns = any(
-        col.startswith("telemetry_diff") for col in df.columns
-    )
+    has_telemetry_diff_columns = any(col.startswith("telemetry_diff") for col in df.columns)
 
     logmsg = f"Number of columns originally: {len(df.columns)}"
     if logger:
@@ -76,9 +78,7 @@ def clean_dataframe(
     if sum_lists:
         # Identify the original columns that were lists or lists of lists
         list_cols = [
-            col
-            for col in dfa.columns
-            if any(isinstance(val, (list, list)) for val in dfa[col])
+            col for col in dfa.columns if any(isinstance(val, (list, list)) for val in dfa[col])
         ]
 
         cols_to_drop = []
@@ -114,40 +114,26 @@ def clean_dataframe(
     if aggregate_telemetry and has_telemetry_diff_columns:
         cols_to_drop = []
 
-        network_cols = [
-            col
-            for col in dfa.columns
-            if col.startswith("telemetry_diff.network")
-        ]
-        dfa["telemetry_diff.network.activity"] = dfa[network_cols].mean(
-            axis=1
-        )
+        network_cols = [col for col in dfa.columns if col.startswith("telemetry_diff.network")]
+        dfa["telemetry_diff.network.activity"] = dfa[network_cols].mean(axis=1)
 
         io_sum_cols = [col for col in dfa.columns if "disk.io_sum" in col]
         dfa["telemetry_diff.disk.activity"] = dfa[io_sum_cols].mean(axis=1)
 
-        processes_nums_cols = [
-            col for col in dfa.columns if "telemetry_diff.process.num_" in col
-        ]
-        dfa["telemetry_diff.process.activity"] = dfa[processes_nums_cols].sum(
-            axis=1
-        )
+        processes_nums_cols = [col for col in dfa.columns if "telemetry_diff.process.num_" in col]
+        dfa["telemetry_diff.process.activity"] = dfa[processes_nums_cols].sum(axis=1)
 
         cols_to_drop.extend(processes_nums_cols)
         cols_to_drop.extend(network_cols)
         cols_to_drop.extend(io_sum_cols)
 
-        cols_to_drop.extend(
-            [col for col in dfa.columns if "disk.io_per_disk" in col]
-        )
+        cols_to_drop.extend([col for col in dfa.columns if "disk.io_per_disk" in col])
 
         dfa.drop(columns=cols_to_drop, inplace=True)
 
     # Removing any leftover cols
     cols_to_drop = [
-        col
-        for col in dfa.columns
-        if "telemetry_at_start" in col or "telemetry_at_end" in col
+        col for col in dfa.columns if "telemetry_at_start" in col or "telemetry_at_end" in col
     ]
     if len(cols_to_drop):
         dfa.drop(columns=cols_to_drop, inplace=True)
@@ -164,11 +150,10 @@ def clean_dataframe(
 
 
 def analyze_correlations(df, method="kendall", threshold=0):
+    """Analyze correlations."""
     # Create a mask to select the upper triangle of the correlation matrix
     correlation_matrix = df.corr(method=method, numeric_only=True)
-    mask = correlation_matrix.where(
-        np.triu(np.ones(correlation_matrix.shape), k=1).astype(bool)
-    )
+    mask = correlation_matrix.where(np.triu(np.ones(correlation_matrix.shape), k=1).astype(bool))
     corrs = []
 
     # Iterate through the selected upper triangle of the correlation matrix
@@ -177,9 +162,7 @@ def analyze_correlations(df, method="kendall", threshold=0):
             pair = (mask.columns[i], mask.columns[j])
             corr = mask.iloc[i, j]  # Get correlation value
             if abs(corr) >= threshold and pair[0] != pair[1]:
-                corrs.append(
-                    (mask.columns[i], mask.columns[j], round(corr, 2))
-                )
+                corrs.append((mask.columns[i], mask.columns[j], round(corr, 2)))
 
     return pd.DataFrame(
         corrs,
@@ -194,6 +177,7 @@ def analyze_correlations_between(
     method="kendall",
     threshold=0,
 ):
+    """Analyze correlations."""
     corr_df = analyze_correlations(df, method, threshold)
     filtered_df = corr_df[
         (
@@ -208,9 +192,8 @@ def analyze_correlations_between(
     return filtered_df
 
 
-def analyze_correlations_used_vs_generated(
-    df: pd.DataFrame, method="kendall", threshold=0
-):
+def analyze_correlations_used_vs_generated(df: pd.DataFrame, method="kendall", threshold=0):
+    """Analyze correlations."""
     return analyze_correlations_between(
         df,
         col_pattern1="used[.]",
@@ -220,9 +203,8 @@ def analyze_correlations_used_vs_generated(
     )
 
 
-def analyze_correlations_used_vs_telemetry_diff(
-    df: pd.DataFrame, method="kendall", threshold=0
-):
+def analyze_correlations_used_vs_telemetry_diff(df: pd.DataFrame, method="kendall", threshold=0):
+    """Analyze correlations."""
     return analyze_correlations_between(
         df,
         col_pattern1="^used[.]*",
@@ -235,6 +217,7 @@ def analyze_correlations_used_vs_telemetry_diff(
 def analyze_correlations_generated_vs_telemetry_diff(
     df: pd.DataFrame, method="kendall", threshold=0
 ):
+    """Analyze correlations."""
     return analyze_correlations_between(
         df,
         col_pattern1="^generated[.]*",
@@ -245,21 +228,19 @@ def analyze_correlations_generated_vs_telemetry_diff(
 
 
 def format_number(num):
+    """Format a number."""
     suffixes = ["", "K", "M", "B", "T"]
     idx = 0
     while abs(num) >= 1000 and idx < len(suffixes) - 1:
         idx += 1
         num /= 1000.0
     formatted = f"{num:.2f}" if num % 1 != 0 else f"{int(num)}"
-    formatted = (
-        formatted.rstrip("0").rstrip(".")
-        if "." in formatted
-        else formatted.rstrip(".")
-    )
+    formatted = formatted.rstrip("0").rstrip(".") if "." in formatted else formatted.rstrip(".")
     return f"{formatted}{suffixes[idx]}"
 
 
 def describe_col(df, col, label=None):
+    """Describe a column."""
     label = col if label is None else label
     return {
         "label": label,
@@ -274,15 +255,14 @@ def describe_col(df, col, label=None):
 
 
 def describe_cols(df, cols, col_labels):
+    """Describe columns."""
     return pd.DataFrame(
-        [
-            describe_col(df, col, col_label)
-            for col, col_label in zip(cols, col_labels)
-        ]
+        [describe_col(df, col, col_label) for col, col_label in zip(cols, col_labels)]
     )
 
 
 def identify_pareto(df):
+    """Identify pareto."""
     datav = df.values
     pareto = []
     for i, point in enumerate(datav):
@@ -292,14 +272,10 @@ def identify_pareto(df):
 
 
 def find_outliers_zscore(row, threshold=3):
-    numeric_columns = [
-        col
-        for col, val in row.items()
-        if pd.api.types.is_numeric_dtype(type(val))
-    ]
+    """Find outliers."""
+    numeric_columns = [col for col, val in row.items() if pd.api.types.is_numeric_dtype(type(val))]
     z_scores = np.abs(
-        (row[numeric_columns] - row[numeric_columns].mean())
-        / row[numeric_columns].std()
+        (row[numeric_columns] - row[numeric_columns].mean()) / row[numeric_columns].std()
     )
     outliers_columns = list(z_scores[z_scores > threshold].index)
     return outliers_columns
