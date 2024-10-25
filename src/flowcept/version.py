8b4a5e786c5079ecf2ecbbf695d86d4a5efb63f0
Renan Souza <contact@renansouza.org>
2024-10-25 12:44:16 -0400

    Manually bumping minor version

--- src/flowcept/version.py
+++ src/flowcept/version.py
@@ -2,4 +2,4 @@
 # This file is supposed to be automatically modified by the CI Bot.
 # The expected format is: <Major>.<Minor>.<Patch>
 # See .github/workflows/version_bumper.py
-__version__ = "0.5.3"
+__version__ = "0.6.0"
