51e78d8f450611ca8b9f1d618ac5bd02a6f2efcd
Renan Souza <contact@renansouza.org>
2024-10-25 12:55:02 -0400

    Fix in version_bumper.py to adhere to docstrings format

--- .github/workflows/version_bumper.py
+++ .github/workflows/version_bumper.py
@@ -9,7 +9,6 @@ split_version = version.split(".")
 old_patch_str = split_version[2]
 re_found = re.findall(r"(\d+)(.*)", old_patch_str)[0]
 old_patch_number = re_found[0]
-# old_branch = re_found[1]
 
 new_patch_str = old_patch_str.replace(
     old_patch_number, str(int(old_patch_number) + 1)
@@ -22,7 +21,10 @@ print("New version: " + new_version)
 
 with open(version_file_path, "w") as f:
     f.write(
-        f"""# WARNING: CHANGE THIS FILE MANUALLY ONLY TO RESOLVE CONFLICTS!
+        f"""
+\"\"\"Version module.\"\"\"
+
+# WARNING: CHANGE THIS FILE MANUALLY ONLY TO RESOLVE CONFLICTS!
 # This file is supposed to be automatically modified by the CI Bot.
 # The expected format is: <Major>.<Minor>.<Patch>
 # See .github/workflows/version_bumper.py
