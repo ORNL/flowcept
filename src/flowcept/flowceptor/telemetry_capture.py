06da889c67074f4aa6d9c4bf272b03b1811c3b40
Renan Souza <contact@renansouza.org>
2024-10-28 16:17:51 -0400

    Fix deprecated warning in telemetry_capture https://github.com/ORNL/flowcept/issues/155

--- src/flowcept/flowceptor/telemetry_capture.py
+++ src/flowcept/flowceptor/telemetry_capture.py
@@ -219,7 +219,7 @@ class TelemetryCapture:
                 p.executable = psutil_p.exe()
                 p.cmd_line = psutil_p.cmdline()
                 p.num_open_file_descriptors = psutil_p.num_fds()
-                p.num_connections = len(psutil_p.connections())
+                p.num_connections = len(psutil_p.net_connections())
                 try:
                     p.io_counters = psutil_p.io_counters()._asdict()
                 except Exception:
