53ee36834e245f354e0e076b8bfe102f721f8d25
Renan Souza <contact@renansouza.org>
2024-11-27 16:40:31 -0500

    Fix in image version

--- deployment/build-image.sh
+++ deployment/build-image.sh
@@ -22,26 +22,13 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Miniconda image built successfully."
-
-# Extract the version from src/version.py
-echo "Extracting version from src/version.py..."
-VERSION=$(python3 -c "from src.flowcept.version import __version__; print(__version__)")
-
-# Check if version was extracted successfully
-if [ -z "$VERSION" ]; then
-    echo "Failed to extract version from src/flowcept/version.py."
-    exit 1
-fi
-
-echo "Extracted Flowcept version: $VERSION"
-
 # Step 4: Build the flowcept image with both 'latest' and versioned tags
 echo "Building flowcept image with latest and version tags..."
-docker build -t flowcept:latest -t flowcept:$VERSION -f deployment/Dockerfile .
+docker build -t flowcept:latest -f deployment/Dockerfile .
 
 # Check if the flowcept build succeeded
 if [ $? -eq 0 ]; then
-    echo "Flowcept image built successfully with tags 'latest' and '$VERSION'."
+    echo "Flowcept image built successfully with tags 'latest'."
     echo "You can now run it using $> make run"
 else
     echo "Failed to build flowcept image."
