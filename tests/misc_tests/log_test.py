59ed46bc9812a587bffffd923b5188115891022c
Renan Souza <contact@renansouza.org>
2025-01-28 17:41:36 -0500

    Fix in flowcept_task

--- tests/misc_tests/log_test.py
+++ tests/misc_tests/log_test.py
@@ -32,18 +32,3 @@ class TestLog(unittest.TestCase):
         self.assertIs(_logger, _logger2)
         _logger.v = "test_val"
         self.assertEqual(_logger2.v, "test_val")
-
-    def test_file_empty(self):
-        if os.path.exists(LOG_FILE_PATH):
-            os.remove(LOG_FILE_PATH)
-            assert not os.path.exists(LOG_FILE_PATH)
-
-        _logger = FlowceptLogger()
-        _logger.debug("Debug Test Msg")
-        _logger.error("Error Test Msg")
-        _logger.critical("Critical Test Msg")
-
-        if LOG_FILE_LEVEL == "DISABLE":
-            assert not os.path.exists(LOG_FILE_PATH), "Log file exists, but it shouldn't."
-        else:
-            assert os.path.exists(LOG_FILE_PATH), "Log file does not exist, but it should."
