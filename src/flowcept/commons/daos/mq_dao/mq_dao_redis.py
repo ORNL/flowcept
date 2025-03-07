f0ba59a999c593c27438617fe33c691af1ef2223
Seth Ockerman <sockerman@wisc.edu>
2025-03-07 17:14:01 +0000

    add timing events to all flushes

--- src/flowcept/commons/daos/mq_dao/mq_dao_redis.py
+++ src/flowcept/commons/daos/mq_dao/mq_dao_redis.py
@@ -4,6 +4,7 @@ from typing import Callable
 
 import msgpack
 from time import time
+import csv
 
 from flowcept.commons.daos.mq_dao.mq_dao_base import MQDao
 from flowcept.commons.utils import perf_log
@@ -22,6 +23,8 @@ class MQDaoRedis(MQDao):
         super().__init__(kv_host, kv_port, adapter_settings)
         self._producer = self._kv_conn  # if MQ is redis, we use the same KV for the MQ
         self._consumer = None
+        self.flush_events = []
+        
 
     def subscribe(self):
         """
@@ -45,12 +48,17 @@ class MQDaoRedis(MQDao):
 
     def send_message(self, message: dict, channel=MQ_CHANNEL, serializer=msgpack.dumps):
         """Send the message."""
+        t1 = time()
         self._producer.publish(channel, serializer(message))
+        t2 = time()
+        self.flush_events.append(["single",t1,t2,t2 - t1, len(str(message).encode())])
 
     def _bulk_publish(self, buffer, channel=MQ_CHANNEL, serializer=msgpack.dumps):
+        total = 0
         pipe = self._producer.pipeline()
         for message in buffer:
             try:
+                total += len(str(message).encode())
                 pipe.publish(MQ_CHANNEL, serializer(message))
             except Exception as e:
                 self.logger.exception(e)
@@ -60,8 +68,11 @@ class MQDaoRedis(MQDao):
         if PERF_LOG:
             t0 = time()
         try:
+            t1 = time()
             pipe.execute()
-            # self.logger.debug(f"Flushed {len(buffer)} msgs to MQ!")
+            t2 = time()
+            self.flush_events.append(["bulk", t1,t2,t2 - t1,total])
+            self.logger.debug(f"Flushed {len(buffer)} msgs to MQ!")
         except Exception as e:
             self.logger.exception(e)
         perf_log("mq_pipe_execute", t0)
@@ -74,3 +85,19 @@ class MQDaoRedis(MQDao):
         except Exception as e:
             self.logger.exception(e)
             return False
+
+    def stop(self,interceptor_instance_id: str, bundle_exec_id: int = None):
+        t1 = time()
+        super().stop(interceptor_instance_id, bundle_exec_id)
+        t2 = time()
+        self.flush_events.append(["final", t1, t2, t2 - t1,'n/a'])
+
+        
+        with open(f"redis_{interceptor_instance_id}_redis_flush_events.csv", "w", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow(["type", "start","end","duration","size"])
+            writer.writerows(self.flush_events)
+        
+        # lets consumer know when to stop
+        self.producer.push(metadata={"message":{"stop-now"}})  # using metadata to send data
+        self.producer.flush()
