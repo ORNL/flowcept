cab7388306109d1fe0a38396c61607747d87f23d
Renan Souza <contact@renansouza.org>
2025-09-03 11:20:21 -0400

    Introduction @flowcept

--- examples/start_here.py
+++ examples/start_here.py
@@ -1,15 +1,16 @@
 """
 This is a very simple script to show the basic instrumentation capabilities of Flowcept, using its most straightforward
-way of capturing workflow provenance from functions and accessing its internal buffer.
+way of capturing workflow provenance from functions: using @decorators.
 
-This very simple scenario does not need any database or external service.
-Flowcept will flush its buffer to a file in the end if a dump_buffer_path is defined in the settings.
+Flowcept will flush its internal buffer to a simple JSONL file in the end, if a `dump_buffer_path` is defined in
+ the settings file (typically under ~/.flowcept/settings.yaml).
 
-For online provenance analysis, HPC requirements, federated/highly distributed execution,
- data observability from existing adapters, PyTorch models, telemetry capture optimization,
- query requirements, or any other provided feature or custom requirements, see the rest of examples/ directory.
+This very simple scenario does not need any database, streaming service, message queue or any other external service.
+It should run fine after installing Flowcept via `pip install flowcept` and running `$> flowcept --init-settings`.
 
-This example's intent is to be simple just to show the most basic utilization scenario for Flowcept.
+For more complex features, such as online provenance analysis, HPC requirements, federated/highly distributed execution,
+ data observability from existing adapters, PyTorch models, telemetry capture optimization, query requirements, or
+ any other provided feature or custom requirements, see the rest of examples/ directory and Flowcept docs.
 
 Note:
 - Adding output_names is not required, but they will make the generated provenance look nicer (and more semantic).
@@ -17,6 +18,7 @@ Note:
 import json
 
 from flowcept import Flowcept, flowcept_task
+from flowcept.instrumentation.flowcept_decorator import flowcept
 
 
 @flowcept_task(output_names="o1")
@@ -29,15 +31,14 @@ def mult_two(o1):
     return o1*2
 
 
+@flowcept
 def main():
     """
     This contains the workflow code.
     """
-
     n = 3
-    with Flowcept(start_persistence=False, save_workflow=False, check_safe_stops=False):
-        o1 = sum_one(n)
-        o2 = mult_two(o1)
+    o1 = sum_one(n)
+    o2 = mult_two(o1)
     print("Final output", o2)
 
 
@@ -48,5 +49,3 @@ if __name__ == "__main__":
     prov_messages = Flowcept.read_messages_file()
     assert len(prov_messages) == 2
     print(json.dumps(prov_messages, indent=2))
-
-
