8b4d109757a81fa0af147bcb1db11716858ae7de
Renan Souza <contact@renansouza.org>
2026-02-03 12:28:08 -0500

    Minor changes in images

--- deployment/build-image.sh
+++ deployment/build-image.sh
@@ -5,26 +5,9 @@ if [ ! -d "src" ]; then
     exit 1
 fi
 
-# Download the Miniconda Dockerfile
-echo "Downloading Miniconda Dockerfile..."
-curl --silent -o Dockerfile_miniconda https://raw.githubusercontent.com/anaconda/docker-images/refs/heads/main/miniconda3/debian/Dockerfile
-cat Dockerfile_miniconda
-
-# Build the Miniconda image locally
-echo "Building miniconda:local image..."
-docker build -t miniconda:local -f Dockerfile_miniconda .
-rm Dockerfile_miniconda
-
-# Check if the Miniconda build failed
-if [ $? -ne 0 ]; then
-    echo "Error: Miniconda image build failed."
-    exit 1
-fi
-
-echo "Miniconda image built successfully."
-# Step 4: Build the flowcept image with both 'latest' and versioned tags
+# Build the flowcept image with latest tag
 echo "Building flowcept image with latest and version tags..."
-docker build --no-cache -t flowcept:latest -f deployment/Dockerfile .
+docker build -t flowcept:latest -f deployment/Dockerfile .
 
 # Check if the flowcept build succeeded
 if [ $? -eq 0 ]; then
@@ -34,4 +17,3 @@ else
     echo "Failed to build flowcept image."
     exit 1
 fi
-
