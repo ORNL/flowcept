93e0c37194ff5f5f819bfdd22be97e14e0a4a708
Renan Souza <contact@renansouza.org>
2026-03-30 15:39:05 -0400

    fix ci

--- .github/workflows/run_examples.sh
+++ .github/workflows/run_examples.sh
@@ -70,7 +70,8 @@ run_test() {
     echo "Installing ml_dev dependencies"
     pip install .[dask] > /dev/null 2>&1
     pip install .[ml_dev]
-    flowcept --init-settings --dask -y
+    flowcept --init-settings --full -y
+    flowcept --config-profile full-online -y
     echo "Defining python path for llm_complex..."
   elif [[ "$test_type" == "unmanaged/simple_task2.py" ]]; then
     export FLOWCEPT_USE_DEFAULT=true
