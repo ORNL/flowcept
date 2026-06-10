db8048f0f7d4f0ac52b0e0dbb5c5abb423621e6f
Renan Souza <contact@renansouza.org>
2026-06-10 13:48:38 -0400

    Reverting unrelated changes

--- examples/instrumented_simple_example.py
+++ examples/instrumented_simple_example.py
@@ -16,16 +16,8 @@ with Flowcept():
     o1 = sum_one(n)
     o2 = mult_two(o1)
     print(o2)
-# docs = Flowcept.db.get_tasks_from_current_workflow()
-# print(len(docs))
-# print(f"Workflow_id={Flowcept.current_workflow_id}")
-# assert len(docs) == 2
-
-provenance_card_path = "PROVENANCE_CARD.md"
-prov_buffer = Flowcept.read_buffer_file()
-report_stats = Flowcept.generate_report(
-    records=prov_buffer,
-    output_path=provenance_card_path,
-)
-print(f"{provenance_card_path} generated!")
+docs = Flowcept.db.get_tasks_from_current_workflow()
+print(len(docs))
+print(f"Workflow_id={Flowcept.current_workflow_id}")
+assert len(docs) == 2
 
